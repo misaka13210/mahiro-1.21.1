72e36d009aaeb0977131b55c06947d8ab6f7d64b
misaka13210 <misaka13210@gmail.com>
2024-08-25 23:10:47 +0800

    对拐杖进行重写（未完成）

--- src/main/java/mahiro76/mahiro/Mahiro.java
+++ src/main/java/mahiro76/mahiro/Mahiro.java
@@ -10,7 +10,7 @@ public class Mahiro implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "Mahiro";
+	public static final String MOD_ID = "mahiro";
     public static final Logger LOGGER = LoggerFactory.getLogger("Mahiro");
 
 	@Override
