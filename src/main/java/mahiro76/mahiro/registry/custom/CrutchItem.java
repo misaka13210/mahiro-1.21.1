30042c446e6172029002d4a321347782f656f4ac
misaka13210 <misaka13210@gmail.com>
2024-08-27 11:53:26 +0800

    对拐杖加入进行完善，添加合成配方，添加物品提示

--- src/main/java/mahiro76/mahiro/registry/custom/CrutchItem.java
+++ src/main/java/mahiro76/mahiro/registry/custom/CrutchItem.java
@@ -7,6 +7,9 @@ import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
@@ -22,9 +25,15 @@ public class CrutchItem extends Item {
         // 这里的标识符是干什么用的？
         Identifier id = Identifier.of(Mahiro.MOD_ID, "crutch_knockback");
         // 拐杖物品的击退属性修改器，1是击退属性的值
-        EntityAttributeModifier modifier = new EntityAttributeModifier(id, 1, EntityAttributeModifier.Operation.ADD_VALUE);
+        EntityAttributeModifier modifier = new EntityAttributeModifier(id, 5, EntityAttributeModifier.Operation.ADD_VALUE);
         // 物品的所有添加的属性，在这里，只添加了击退一种属性
         List<AttributeModifiersComponent.Entry> entries = List.of(new AttributeModifiersComponent.Entry(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, modifier, AttributeModifierSlot.MAINHAND));
         return new AttributeModifiersComponent(entries, true);
+    }
+
+    //覆写方法，添加物品提示文本
+    @Override
+    public void appendTooltip(ItemStack itemStack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("item.mahiro.crutch.tips"));
     }
 }
